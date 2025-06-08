8caeceecae0ecf6c05569160110b81778893900c
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-06-07 23:40:06 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
+author 'PrinceAlbert (Noor Nahas) of Popcorn RP'
+description 'Aquatic Ped Survival Script'
+version '1.0.0'
+
+client_script 'client.lua'
